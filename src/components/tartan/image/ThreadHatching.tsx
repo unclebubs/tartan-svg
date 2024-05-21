84ca967ef1dfc473919aaad96291e2ee4b634c19
Craig Callaghan <craig.callaghan@gmail.com>
2024-05-21 15:41:21 +0100

    added test id for testing

--- src/components/tartan/image/ThreadHatching.tsx
+++ src/components/tartan/image/ThreadHatching.tsx
@@ -6,7 +6,7 @@ const ThreadHatching: React.FC = () => {
   const x = (tartan === null ? 2 : tartan.scaledStrokeLength)
 
   return (
-    <pattern id="threadHatching" x="0" y="0" patternUnits="userSpaceOnUse" width={x * 2} height={x * 2}>
+    <pattern data-testid='pattern' id="threadHatching" x="0" y="0" patternUnits="userSpaceOnUse" width={x * 2} height={x * 2}>
       <rect x="0" y={-x / 2} height={x} width={x / 2} fill="white"></rect>
       <rect x="0" y={x * 1.5} height={x} width={x / 2} fill="white"></rect>
       <rect x={x / 2} y="0" height={x} width={x / 2} fill="white"></rect>
